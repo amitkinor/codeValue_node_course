0afa8756b687f6ee19e63db6238164b56ebcaef4
Amit Kinor <amit.kinor@gmail.com>
2020-11-07 21:38:28 +0200

    fixed reject , resolve mistake

--- ex1/index.js
+++ ex1/index.js
@@ -5,13 +5,10 @@ const { eventEmitter, init } = require("./source");
 let counter = 1;
 
 eventEmitter.on(constants.SHOOT_NUMBERS, (data) => {
-	console.log(`Calculation number ${counter} : `);
 	counter++;
 
 	sum(data.num1, data.num2)
-		.then((result) =>
-			console.log(result)
-		)
+		.then((result) => console.log(`Calculation number ${counter} : ${result}`))
 		.catch((err) => {
 			console.log(err);
 		});
